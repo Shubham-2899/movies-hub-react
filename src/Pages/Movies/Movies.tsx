97c4dd3db885d9c439235df2f9bd13ec9fbfae10
Shubham-2899 <shubhamlatake123@gmail.com>
2022-06-02 12:53:43 +0530

    api key corrected

--- src/Pages/Movies/Movies.tsx
+++ src/Pages/Movies/Movies.tsx
@@ -19,7 +19,7 @@ const Movies = () => {
     isLoading,
     error,
   } = useFetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=8802ce576b2e68c5d93fa88c487234f7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`,
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`,
     genreforURL
   );
 
